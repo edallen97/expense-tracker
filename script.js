53505b6ea7791576af06270036377f8cb3f0995d
edallen97 <126800045+edallen97@users.noreply.github.com>
2023-03-24 17:31:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
 // selectors
+const addExpenseButton = document.querySelector('.addExpense')
+
+// name
 const nameInput = document.querySelector('.nameInput')
 const nameSubmitButton = document.querySelector('.nameSubmit')
 const nameDiv = document.querySelector('.nameOutput')
 
+// amount
+const amountInput = document.querySelector('.amountInput')
+const amountSubmitButton = document.querySelector('.amountSubmit')
+const amountDiv = document.querySelector('.amountOutput')
+
+// date
+const dateInput = document.querySelector('.dateInput')
+const dateSubmitButton = document.querySelector('.dateSubmit')
+const dateDiv = document.querySelector('.dateOutput')
+
+
 // event listeners
-nameSubmitButton.addEventListener('click', test)
+//nameSubmitButton.addEventListener('click', addName)
+//amountSubmitButton.addEventListener('click', addAmount)
+//dateSubmitButton.addEventListener('click', addDate)
+
+addExpenseButton.addEventListener('click', addExpense)
 
 // functions
-function test() {
-   // console.log(nameInput.value)
-   let nameValue = document.createElement('ol');
-   nameValue.innerText = nameInput.ariaValueMax;
-   nameDiv.appendChild(nameValue);
 
-} 
+// adds name to div
+function addExpense() {
+   function addName() {
+      // console.log(nameInput.value)
+   const nameValue = document.createElement('ol');
+   nameValue.innerText = nameInput.value;
+   nameDiv.appendChild(nameValue);
+   } 
+
+   function addAmount() {
+      // console.log(nameInput.value)
+   const amountValue = document.createElement('ol');
+   amountValue.innerText = amountInput.value;
+   amountDiv.appendChild(amountValue);
+   } 
+   function addDate() {
+      // console.log(nameInput.value)
+   const dateValue = document.createElement('ol');
+   dateValue.innerText = dateInput.value;
+   dateDiv.appendChild(dateValue);
+   } 
+}
